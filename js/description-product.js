0bbf8db17c8ecc4d3f315815c8f1d86f7a466814
Fernando Pavanello <fernando.pavanello2@gmail.com>
2018-12-19 21:28:31 -0200

    [013] - Desafio Intermediário

--- js/description-product.js
+++ js/description-product.js
@@ -2,7 +2,12 @@ const $heart = window.document.querySelector(".-heart");
 const $addCart = window.document.querySelector(".button-store.-second");
 
 $heart.addEventListener("click", handleClick);
+$addCart.addEventListener("click", handleButtonCart);
 
 function handleClick() {
   console.log("Salve");
+}
+
+function handleButtonCart() {
+  console.log("Comprou");
 }
