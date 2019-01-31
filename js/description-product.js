dbb4b8176eba83c2025a1a093c4ce1605d7967ae
Fernando Pavanello <fernando.pavanello2@gmail.com>
2019-01-30 22:02:18 -0200

    Aula 14 - Desafio Intermediário

--- js/description-product.js
+++ js/description-product.js
@@ -1,7 +1,19 @@
 const $heart = document.querySelector(".-heart");
+const $stars = document.querySelector(".-star").childNodes;
 
 $heart.addEventListener("click", handleClick);
 
 function handleClick() {
   $heart.classList.toggle("-active");
+}
+
+for (var i = 0; i < $stars.length; i++) {
+  $stars[i].addEventListener("click", function(){
+    if (this.src.includes("img/star.png")) {
+      this.src = "img/star-active.png";
+      controlStars($stars, i);
+    } else {
+      this.src = "img/star.png";
+    }
+  });
 }
