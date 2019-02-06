5cd8090809c0c1607add66e88a6d577a5af65e7c
Fernando Pavanello <fernando.pavanello2@gmail.com>
2019-02-06 20:14:02 -0200

    Fim aula 016 - Preenchimento das estrelas

--- js/description-product.js
+++ js/description-product.js
@@ -1,28 +1,45 @@
 const $heart = document.querySelector(".-heart");
 const $stars = document.querySelectorAll(".star");
+const positionLast = $stars.length - 1;
 
-$heart.addEventListener("click", handleClickHeart);
+$heart.addEventListener("click", handleClick);
 
-function handleClickHeart() {
+$stars.forEach(function($star, key) {
+  if(key == 0) {
+    $star.addEventListener("click", firstStar);
+
+  } else if(key == positionLast) {
+    $star.addEventListener("click", lastStar);
+
+  } else {
+    $star.addEventListener("click", function() {
+      middleStar(key);
+    })
+  }
+})
+
+function handleClick() {
   this.classList.toggle("-active");
 }
 
-$stars.forEach(function($star) {
-  $star.addEventListener('click', handleClickStars);
-});
-
-function handleClickStars() {
-  this.classList.toggle("-active");
-  controlStars(this.id);
-}
-
-function controlStars(selectId) {  
+function firstStar() {
   $stars.forEach(function($star) {
-    if(selectId >= $star.id && !$star.classList.contains("-active")) {
-      $star.className += " -active";
+    $star.classList.remove("-active");
+  });
+}
+
+function middleStar(index) {
+  $stars.forEach(function($star, key) {
+    $star.classList.remove("-active");
+
+    if(key <= index) {
+      $star.classList.add("-active");
     }
-    if(selectId < $star.id && $star.classList.contains("-active")) {
-      $star.classList.remove("-active");
-    }
+  });
+}
+
+function lastStar() {
+  $stars.forEach(function($star) {
+    $star.classList.add("-active");
   });
 }
