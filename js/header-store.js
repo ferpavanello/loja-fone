0d282447ac0fc097a3eb1b726bd4e79f7c792c32
Fernando <fernando.pavanello2@gmail.com>
2019-02-13 08:20:00 +0000

    Aula 018 - Fim (Buscar responsivo)

--- js/header-store.js
+++
@@ -1,7 +0,0 @@
-const $search = document.querySelector('[type=search]');
-const $imgSearch = document.querySelector('.img-search');
-
-$imgSearch.addEventListener('click', () => {
-    $imgSearch.classList.toggle("-active");
-    $search.classList.toggle("-active");
-});
